e0c710ad9e369ffe01f1332fc7839d73d9454e60
Ulaş Can Demirbağ <ulascandemirbag@gmail.com>
2021-09-07 21:40:02 +0300

    tweet list tasarımı tamamlandı

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="text-white">
-    <div class="q-py-lg q-px-md row items-end q-col-gutter-md ">
+    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
       <div class="col">
         <q-input
           dark
@@ -30,7 +30,7 @@
           </template>
         </q-input>
       </div>
-      <div class="col col-shrink ">
+      <div class="col col-shrink">
         <q-btn
           :disable="!newTweetContent"
           unelevated
@@ -42,7 +42,40 @@
         />
       </div>
     </div>
-            <q-separator class="bg-grey-8" />
+    <q-separator class="bg-grey-8" />
+    <q-list>
+      <q-item class="q-py-md">
+        <q-item-section avatar top>
+          <q-avatar size="xl">
+            <img src="https://avatars.githubusercontent.com/u/39746408?v=4" />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-subtitle1"
+            ><strong>Ulaş Can Demirbağ </strong>
+            <span class="text-grey-7"> @ulascan54</span>
+          </q-item-label>
+          <q-item-label class="tweet-content text-body-one">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste
+            possimus nam voluptatem pariatur obcaecati dolore, maiores facilis
+            illo dolores eaque quos facere temporibus repudiandae repellat
+            itaque qui. 
+            Quis, aliquid quod!
+          </q-item-label>
+          <div class="row justify-between q-mt-sm tweet-icons">
+                <q-btn flat round color="grey-7" icon="far fa-comment" size="sm" class="btn-action"/>
+                <q-btn flat round color="grey-7" icon="fas fa-retweet" size="sm" class="btn-action"/>
+                <q-btn flat round color="grey-7" icon="fas fa-heart" size="sm" class="btn-action"/>
+                <q-btn flat round color="grey-7" icon="fas fa-trash" size="sm" class="btn-action"/>
+          </div>
+        </q-item-section>
+
+        <q-item-section side top> 1 min ago </q-item-section>
+      </q-item>
+
+      <q-separator class="bg-grey-8" />
+    </q-list>
   </q-page>
 </template>
 
@@ -61,4 +94,13 @@ export default {
   textarea
     font-size:19px
     line-height:1.4 !important
+
+.tweet-content
+  white-space: pre-line !important
+
+.btn-action:hover
+  color: white !important
+
+.tweet-icons
+  margin-left: -5px
 </style>
