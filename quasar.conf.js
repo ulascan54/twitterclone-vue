01ba7090bfa2db5bca4838df3d5241befbd96254
Ulaş Can Demirbağ <ulascandemirbag@gmail.com>
2021-09-08 18:35:24 +0300

    proje tamamlandı

--- quasar.conf.js
+++ quasar.conf.js
@@ -44,9 +44,9 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'history', // available values: 'hash', 'history'
-
+      vueRouterMode: 'hash', // available values: 'hash', 'history'
       // transpile: false,
+      distDir: ctx.mode.spa ? 'public' : null,
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
